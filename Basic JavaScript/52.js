8abeb7ae19a9bc14aaf2c2eff5c7010404973c99
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-03-16 00:23:14 +0530

    Return a Value from a Function with Return

---
+++ Basic JavaScript/52.js
@@ -0,0 +1,12 @@
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num0) {
+  return num0*5;
+}
+
+
+console.log(minusSeven(10));
