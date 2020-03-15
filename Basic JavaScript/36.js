817d79213c66deda294cd940d3800c06e02eebad
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-03-16 00:03:30 +0530

    Word Blanks

---
+++ Basic JavaScript/36.js
@@ -0,0 +1,8 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = " I am " + myNoun + " and we are " + myAdjective + " and we " + myVerb + " fast and " + myAdverb;; // Change this line
+// Only change code above this line
