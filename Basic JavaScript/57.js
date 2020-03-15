d8dc696086b228e65377c35f39f2b657f753b456
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-03-16 00:30:17 +0530

    Use Conditional Logic with If Statements

---
+++ Basic JavaScript/57.js
@@ -0,0 +1,25 @@
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+
+  // Only change code below this line
+if (wasThatTrue) {
+  return "Yes, that was true";
+}
+else {
+  return "No, that was false";
+}
+
+
+  // Only change code above this line
+
+}
+
+trueOrFalse(true);
