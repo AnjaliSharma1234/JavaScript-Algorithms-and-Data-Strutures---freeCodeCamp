150ffa54099953b18f6d6877c3363b0a907891ba
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-03-16 00:18:53 +0530

    Passing Values to Functions with Arguments

---
+++ Basic JavaScript/48.js
@@ -0,0 +1,13 @@
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line
+function functionWithArgs(arg1, arg2) {
+  console.log(arg1 + arg2);
+
+}
+
+functionWithArgs(7,3);
