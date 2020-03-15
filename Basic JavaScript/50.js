008e9195f94d5282b6fb0604ef58d40c4f455dce
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-03-16 00:21:15 +0530

    Local Scope and Functions

---
+++ Basic JavaScript/50.js
@@ -0,0 +1,13 @@
+function myLocalScope() {
+  'use strict';
+
+  // Only change code below this line
+  var myVar = 10;
+  console.log(myVar);
+}
+myLocalScope();
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log(myVar);
+
+// Now remove the console log line to pass the test
