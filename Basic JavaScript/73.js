38d3a0c19d5739a500cb539422b4087526d0c465
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-03-17 23:30:03 +0530

    Selecting from Many Options with Switch Statements

---
+++ Basic JavaScript/73.js
@@ -0,0 +1,16 @@
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+switch(val){
+  case 1: return "alpha"; break;
+  case 2: return "beta"; break;
+  case 3: return "gamma"; break;
+  case 4: return "delta"; break;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
