bfad1cd59726d54dd7ccfaf96993c09d215d6f32
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-03-18 12:34:56 +0530

    Multiple Identical Options in Switch Statements

---
+++ Basic JavaScript/75.js
@@ -0,0 +1,21 @@
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val) {
+    case 1:
+    case 2:
+    case 3: return "Low"; break;
+    case 4:
+    case 5:
+    case 6: return "Mid"; break;
+    case 7:
+    case 8:
+    case 9: return "High"; break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
