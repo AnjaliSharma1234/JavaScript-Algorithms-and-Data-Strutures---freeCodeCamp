3916f21860d3f0aef87b086848bdd97f63d95d43
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-03-19 23:40:50 +0530

    Manipulating Complex Objects

---
+++ Basic JavaScript/90.js
@@ -0,0 +1,20 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add a record here
+  {
+    "artist": "Anjali",
+    "title": "Fucked up Anjali",
+    "release_year": 1998,
+    "formats": [ "Cd", "life", "death"]
+  }
+];
