390e3a042ecbb2a57499c56826c67cb268ce6404
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-03-20 18:02:32 +0530

    Iterate with JavaScript For Loops

---
+++ Basic JavaScript/95.js
@@ -0,0 +1,14 @@
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}
