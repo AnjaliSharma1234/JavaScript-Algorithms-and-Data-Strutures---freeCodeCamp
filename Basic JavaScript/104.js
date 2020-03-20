59d2227e322e4b7a1fca5f77372b8eaf778815c3
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-03-20 19:31:24 +0530

    Generate Random Whole Numbers with JavaScript

---
+++ Basic JavaScript/104.js
@@ -0,0 +1,8 @@
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line
+
+  return Math.floor(Math.random() * 10);
+}
