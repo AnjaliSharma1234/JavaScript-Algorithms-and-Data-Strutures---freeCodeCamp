fa811cf3ee676f6f057b1216e39c04eb5808b882
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-03-20 18:00:51 +0530

    Record Collection

---
+++ Basic JavaScript/93.js
@@ -0,0 +1,41 @@
+// Setup
+var collection = {
+  2548: {
+    album: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: [
+      "Let It Rock",
+      "You Give Love a Bad Name"
+    ]
+  },
+  2468: {
+    album: "1999",
+    artist: "Prince",
+    tracks: [
+      "1999",
+      "Little Red Corvette"
+    ]
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [ ]
+  },
+  5439: {
+    album: "ABBA Gold"
+  }
+};
+
+// Only change code below this line
+function updateRecords(id, prop, value) {
+  if(value === "") delete collection[id][prop];
+  else if(prop === "tracks") {
+    collection[id][prop] = collection[id][prop] || [];
+    collection[id][prop].push(value);
+  } else {
+    collection[id][prop] = value;
+  }
+
+  return collection;
+}
+
+updateRecords(5439, "artist", "ABBA");
