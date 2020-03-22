6cf7547a1e41736ffa22694dffe9b032b1b4fc1d
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-03-22 23:48:15 +0530

    Declare a Read-Only Var with the const Keyword

---
+++ ES6/03.js
@@ -0,0 +1,14 @@
+function printManyTimes(str) {
+  "use strict";
+
+  // Only change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for (let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // Only change code above this line
+
+}
+printManyTimes("freeCodeCamp");
