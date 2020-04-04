111298f01d3d89b19a39d635060374cd47936d54
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-04-04 21:58:58 +0530

    Differences btw freeCodeCamp and Browser Console

---
+++ Debugging/02.js
@@ -0,0 +1,11 @@
+// Open your browser console.
+let output = "Get this to log once in the browser console and twice in the freeCodeCamp console";
+// Use console.clear() on the next line to clear the browser console.
+console.clear()
+
+// Use console.log() to print the output variable.
+console.log(output);
+
+// Check the two consoles to see the difference. The freeCodeCamp console should have printed the variable twice, once for each test of this challenge. The browser console should only print the variable once because you cleared it first.
+
+
