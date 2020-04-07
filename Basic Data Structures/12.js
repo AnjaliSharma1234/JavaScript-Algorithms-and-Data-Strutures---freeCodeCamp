a16c5ce064fa1b9ef7983cba1f133a64fe6fa86e
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-04-07 18:37:08 +0530

    Create complex multi-D arrays

---
+++ Basic Data Structures/12.js
@@ -0,0 +1,22 @@
+let myNestedArray = [
+  // Only change code below this line
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+  [
+    ['loop', 'shift', 6, 7, 1000, 'method', 'deep']
+  ],
+  [
+    [
+        ['concat', false, true, 'spread', 'array', 'deeper']
+    ],
+    [
+      [
+
+        ['mutate', 1327.98, 'splice', 'slice', 'push'],
+        ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth', 'deepest']
+      ]
+    ]
+  ]
+  
+  
+  // Only change code above this line
+];
